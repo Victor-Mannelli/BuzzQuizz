947be1ae3b0b9b738af7a79fea6061ef323cd2ad
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-08-31 15:44:24 -0300

    refactor: creation of functions to render elements

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-const quizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-quizzes.then(addAllQuizzes);
-quizzes.catch((error => console.log(`erro: ${error.response.data}`)));
+let quizzesUser = [];
+let quizzesOtherUsers = [];
 
-function addAllQuizzes(quizzesResposta){
-    console.log(quizzesResposta.data.image)
-    const todosOsQuizzes = document.querySelector('.all-quizzes ul');
-    quizzesResposta.data.forEach(element => { 
-        todosOsQuizzes.innerHTML += `
-        <li>
-            <img src="${element.image}" alt=""></img>
-        </li>
-    `
-    });
+function searchQuizzes() {
+    const response = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    response.then(processResponse);
+    response.catch((error => console.log(`erro: ${error.response.data}`)));
 }
+
+function processResponse(response) {
+    quizzesOtherUsers = response.data;
+    renderQuizzes();
+}
+
+function quizzesTagLi(quizzes) {
+    let quizzesLI = '';
+    quizzes.forEach(quizz => { 
+        quizzesLI += `
+            <li>
+                <img src="${quizz.image}" alt=""></img>
+            </li>
+        `;
+    });
+    return quizzesLI;
+}
+
+function renderQuizzes() {
+    const main = document.querySelector('main');
+    //quizzesUser = [quizzesOtherUsers[30]];
+    if (quizzesUser.length === 0) {
+        main.innerHTML = `
+            <section class="creating-quizz">
+                <p>Você não criou nenhum <br> quizz ainda :(</p>
+                <button>Criar Quizz</button>
+            </section>
+        `;
+    } else {
+        main.innerHTML = `
+            <section class="user-quizzes">
+                <div>
+                    <h1>Seus Quizzes</h1>
+                    <ion-icon name="add-circle-sharp"></ion-icon>
+                </div>
+                <ul>${quizzesTagLi(quizzesUser)}</ul>
+            </section>
+        `;
+    }
+
+    main.innerHTML += `
+        <section class="other-user-quizzes">
+            <div>
+                <h1>Todos os Quizzes</h1>
+            </div>
+            <ul>${quizzesTagLi(quizzesOtherUsers)}</ul>
+        </section>
+        `;
+}
+
+searchQuizzes();
