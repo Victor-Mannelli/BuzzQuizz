e3ca79ec45aff7c1be531c9286be8ff51f1759c4
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-08-31 21:56:16 -0300

    feat creating quizz screen

--- script.js
+++ script.js
@@ -4,32 +4,36 @@ let quizzesOtherUsers = [];
 // SCREEN 1
 
 function searchQuizzes() {
-    const response = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-    response.then(responseQuizzes);
-    response.catch((error => console.log(`erro: ${error.response.data}`)));
+  const response = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  response.then(responseQuizzes);
+  response.catch((error) => console.log(`erro: ${error.response.data}`));
 }
 
 function separateQuizzes(quizzes) {
-    let userQuizzesIds = localStorage.getItem('quizzes');
-    if (userQuizzesIds !== null) {
-        userQuizzesIds = JSON.parse(userQuizzesIds);
-        quizzes = quizzes.filter(quizze => userQuizzesIds.includes(quizze.id))
-        return quizzes;
-    } else {
-        return [];
-    }
+  let userQuizzesIds = localStorage.getItem("quizzes");
+  if (userQuizzesIds !== null) {
+    userQuizzesIds = JSON.parse(userQuizzesIds);
+    quizzes = quizzes.filter((quizze) => userQuizzesIds.includes(quizze.id));
+    return quizzes;
+  } else {
+    return [];
+  }
 }
 
 function responseQuizzes(response) {
-    quizzesUser = separateQuizzes(response.data);
-    quizzesOtherUsers = response.data.filter(quizz => !(quizzesUser.includes(quizz.id)));
-    renderQuizzes();
+  quizzesUser = separateQuizzes(response.data);
+  quizzesOtherUsers = response.data.filter(
+    (quizz) => !quizzesUser.includes(quizz.id)
+  );
+  renderQuizzes();
 }
 
 function insertQuizzes(quizzes) {
-    let quizzesLI = '';
-    quizzes.forEach(quizz => { 
-        quizzesLI += `
+  let quizzesLI = "";
+  quizzes.forEach((quizz) => {
+    quizzesLI += `
             <li class="quizz" onclick="searchQuizz(${quizz.id})">
                 <div class="overlay">
                     <h1>${quizz.title}</h1>
@@ -37,21 +41,21 @@ function insertQuizzes(quizzes) {
                 <img src="${quizz.image}" alt="${quizz.title}"></img>
             </li>
         `;
-    });
-    return quizzesLI;
+  });
+  return quizzesLI;
 }
 
 function renderQuizzes() {
-    const main = document.querySelector('main');
-    if (quizzesUser.length === 0) {
-        main.innerHTML = `
+  const main = document.querySelector("main");
+  if (quizzesUser.length === 0) {
+    main.innerHTML = `
             <section class="creating-quizz">
                 <p>Você não criou nenhum <br> quizz ainda :(</p>
                 <button onclick="addQuizz()">Criar Quizz</button>
             </section>
         `;
-    } else {
-        main.innerHTML = `
+  } else {
+    main.innerHTML = `
             <section class="user-quizzes">
                 <div>
                     <h1>Seus Quizzes</h1>
@@ -60,9 +64,9 @@ function renderQuizzes() {
                 <ul>${insertQuizzes(quizzesUser)}</ul>
             </section>
         `;
-    }
+  }
 
-    main.innerHTML += `
+  main.innerHTML += `
         <section class="other-user-quizzes">
             <div>
                 <h1>Todos os Quizzes</h1>
@@ -77,50 +81,52 @@ searchQuizzes();
 // SCREEN 2
 
 function searchQuizz(id) {
-    const response = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
-    response.then(responseQuizz);
-    response.catch((error => console.log(`erro: ${error.response.data}`)));
+  const response = axios.get(
+    `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`
+  );
+  response.then(responseQuizz);
+  response.catch((error) => console.log(`erro: ${error.response.data}`));
 }
 
 function responseQuizz(response) {
-    renderQuizz(response.data);
+  renderQuizz(response.data);
 }
 
 function insertAnswers(answers) {
-    let answersLI = '';
-    answers.forEach(answer => { 
-        answersLI += `
+  let answersLI = "";
+  answers.forEach((answer) => {
+    answersLI += `
             <li class="answer">
                 <img src="${answer.image}" alt="${answer.text}"></img>
                 <h1>${answer.text}</h1>
             </li>
         `;
-    });
-    return answersLI;
+  });
+  return answersLI;
 }
 
 function insertQuestions(questions) {
-    let questionsLI = '';
-    questions.forEach(question => { 
-        questionsLI += `
+  let questionsLI = "";
+  questions.forEach((question) => {
+    questionsLI += `
             <li class="question">
                 <div class="content">
                     <div class="question-title">
                         <h1>${question.title}</h1>
                     </div>
                     <div class="answers">
-                        <ul>${(insertAnswers(question.answers))}</ul>
+                        <ul>${insertAnswers(question.answers)}</ul>
                     </div>
                 </div>
             </li>
         `;
-    });
-    return questionsLI;
+  });
+  return questionsLI;
 }
 
 function renderQuizz(quizz) {
-    const container = document.querySelector('container');
-    container.innerHTML = `
+  const container = document.querySelector("container");
+  container.innerHTML = `
         <header>BuzzQuizz</header>
             <section class="header-quizz">
                 <div class="overlay">
@@ -139,6 +145,22 @@ function renderQuizz(quizz) {
 // SCREEN 3
 
 function addQuizz() {
-    console.log("Relaxa, vai sair");
-    //localStorage.setItem('quizzes', `[${quizz.id}, ${quizz.id}, ...]`);
+  console.log("Relaxa, vai sair");
+  //localStorage.setItem('quizzes', `[${quizz.id}, ${quizz.id}, ...]`);
+
+  const main = document.querySelector("main");
+
+  main.innerHTML = `<div class="creation-screen">
+  <h1>Comece pelo começo</h1>
+    <div class="box">
+    <ul>
+        <li><input placeholder="Título do seu quizz" type="text"></li>
+        <li><input placeholder="URL da imagem do seu quizz" type="text"></li>
+        <li><input placeholder="Quantidade de perguntas do quizz" type="text"></li>
+        <li><input placeholder="Quantidade de níveis do quizz" type="text"></li>
+        </ul>
+    </div>
+    <button>Prosseguir para criar perguntas</button>
+    </div>
+    `;
 }
