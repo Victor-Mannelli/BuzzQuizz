39a091482b3fac5898b2ce973cbba6940c768247
Victor Mannelli <imp.mannelli@gmail.com>
2022-09-03 23:56:13 -0300

    fix: merge

--- js/answerBehavior.js
+++ js/answerBehavior.js
@@ -11,7 +11,7 @@ function selectedAnswer(selector){
             answers[i].classList.add('non-chosen-answers')
         }
     }
-    endOfQuizz();
+    endOfQuizz(quizzData);
     setTimeout(scrollWithOrder, 2000)
 
     if (selector.classList.contains('true')){
@@ -27,53 +27,57 @@ function scrollWithOrder(){
     } else {
         question[counter].scrollIntoView({ behavior: 'smooth', block: "center"});
     }
-    
 }
 
-function endOfQuizz(value) {
+let quizzData = [];
+function endOfQuizz(levels) {
+    quizzData = levels;
     const question = document.querySelectorAll('.question');
     const questions = document.querySelector('.questions ul');
-    const differentLevels = [];
+    const differentValues = [];
 
     if (counter === question.length) {
-
-        for (let i = 0; i < value.levels.length; i++){
-            differentLevels.push(value.levels[i].minValue);
+        
+        for (let i = 0; i < levels.length; i++){
+            differentValues.push(levels[i].minValue);
         } 
-        differentLevels.sort();
-    
-        const percent = Math.round(correctAnswers/question.length); 
+        
+        const percentOfCorrectAnswers = Math.round((correctAnswers/question.length * 100)); 
+        let valuesUnderPercent = differentValues.filter((level) => percentOfCorrectAnswers > level);
+        let highestValueUnderPercent = valuesUnderPercent[valuesUnderPercent.length]
 
-        for (let i = 0; i < differentLevels.length; i++){
-            if (percent < differentLevels[i]){
-                
+        for (let i = 0; i < levels.length; i++){
+            if (highestValueUnderPercent === levels[i].minValue){
+                highestValuePosition = i
             }
         }
 
-        if(value.levels.minValue )
-            questions.innerHTML += `
+        
+        questions.innerHTML += `
             <li class="question">
                 <div class="feedback-content">
                     <div class="feedback-header">
-                        <h1>${value.levels[i].title}</h1>
+                        <h1>${levels[highestValuePosition].title}</h1>
                     </div>
                     <div class="feedback-main"">
-                        <img src="${value.levels[i].image}"> </img>
-                        <div class="paragraph"> <p> ${value.levels[i].text} </p> </div>
+                        <img src="${levels[highestValuePosition].image}"> </img>
+                        <div class="paragraph"> <p> ${levels[highestValuePosition].text} </p> </div>
                     </div>
                     <div class="feedback-buttons">
                         <button class="re-start" onclick="searchQuizz(idCurrentQuiz)" > Reiniciar Quizz </button>
                         <button onclick="location.reload()" class="back-home"> Voltar para home </button>
                     </div>
-                 </div>
-              </li>
-            `;
-        }
-    
+                </div>
+            </li>
+        `;
+     
         const feedback = document.querySelector('.feedback-content')
         feedback?.scrollIntoView({ behavior: 'smooth'});
 
         const restartButton = document.querySelector('.re-start')
         restartButton.addEventListener("click", (() => counter = 0));
+
+    }
 }
+
 
